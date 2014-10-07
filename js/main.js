a287f8a3bd38677c5b8d6ff1c38e67eb2dcf1de6
sonalsharma <sonalsharma@live.ca>
2014-10-07 09:18:08 -0400

    added dipper

--- js/main.js
+++ js/main.js
@@ -1,5 +1,12 @@
 var $win = $(window);
+var $sun = $('.sun');
+var $sunSeection = $('.sun-section');
 
 $win.on('scroll', function () {
-	console.log('Scrolling');
+	var scrollPos = $win.scrollTop();
+	
+	$sun.css('transform', 'rotate(' + scrollPos / 5 + 'deg)');
+
+	$sunSeection.css('background-position', 'center ' + scrollPos / 2 + 'px')
+
 });
