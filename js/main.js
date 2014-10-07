ffa72881ad4e462a8ddac642448266792545369f
sonalsharma <sonalsharma@live.ca>
2014-10-07 10:25:13 -0400

    added shuttle

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 var $win = $(window);
 var $sun = $('.sun');
 var $sunSeection = $('.sun-section');
+var $dipperSection = $('.dipper-section');
+var $dipper = $('.dipper');
+var $shuttleSection = $('.shuttle-section');
+var $shuttle = $('.shuttle');
 
 $win.on('scroll', function () {
 	var scrollPos = $win.scrollTop();
@@ -8,5 +12,12 @@ $win.on('scroll', function () {
 	$sun.css('transform', 'rotate(' + scrollPos / 5 + 'deg)');
 
 	$sunSeection.css('background-position', 'center ' + scrollPos / 2 + 'px')
-
 });
+
+$dipperSection.waypoint(function () {
+	$dipper.addClass('js-dipper-fade');
+}, { offset:'50%' });
+
+$shuttleSection.waypoint(function () {
+	$shuttle.addClass('js-shuttle-fade');
+}, {offset:'50%'});
